5393a7f6ed4b175c973ab8fbd809aea8961e4bf7
David Smith <david.smith@weaveability.com>
2019-12-19 16:02:06 +0000

    update course page to use course store

--- src/components/CoursesPage.js
+++ src/components/CoursesPage.js
@@ -2,12 +2,16 @@ import React, { useState, useEffect } from "react";
 import CourseStore from "../stores/CourseStore";
 import CourseList from "./CourseList";
 import { Link } from "react-router-dom";
+import { loadCourses } from "../actions/courseActions";
 
 function CoursesPage() {
   const [courses, setCourses] = useState([]);
 
   useEffect(() => {
     CourseStore.addChangeListener(onChange);
+    if (CourseStore.getCourses().length === 0) loadCourses();
+
+    return () => CourseStore.removeChangeListener(onChange); //cleanup on unmount
   }, []); //run once
 
   function onChange() {
